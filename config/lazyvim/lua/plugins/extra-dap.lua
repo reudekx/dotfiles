5fa68927874f42a49e121fee627325ef37b29c6d
reudekx <reudekx@gmail.com>
2024-07-06 14:38:41 +0900

    feat: add some plugins and edit configurations

---
+++ config/lazyvim/lua/plugins/extra-dap.lua
@@ -0,0 +1,13 @@
+return {
+  "jay-babu/mason-nvim-dap.nvim",
+  opts = {
+    automatic_installation = true,
+
+    handlers = {},
+
+    ensure_installed = {
+      "debugpy",
+      "cpptools",
+    },
+  },
+}
