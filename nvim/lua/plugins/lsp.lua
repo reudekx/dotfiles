b5dd064c86dabd9ae09d74286efce0eebf7fc8df
reudekx <reudekx@gmail.com>
2024-07-06 17:35:49 +0900

    feat: fix to handle python virtual envrionment correctly

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,28 @@
 local map_key = require('utils.key-mapper').map_key
 
+local langs = {
+	'lua_ls',
+	'clangd',
+	'pyright',
+	'autotools_ls',
+	'marksman',
+}
+
+local function get_python_path()
+	local venv_path = os.getenv("VIRTUAL_ENV")
+	if venv_path then
+		return venv_path .. 'bin/python'
+	else
+		local handle = io.popen('which python')
+		if not handle then
+			return 'usr/sbin/python'
+		end
+		local result = handle:read("*a")
+		handle:close()
+		return result:gsub("%s+", "")
+	end
+end
+
 return {
 	{
 		'williamboman/mason.nvim',
@@ -12,13 +35,7 @@ return {
 		dependencies = { 'williamboman/mason.nvim' },
 		config = function()
 			require('mason-lspconfig').setup({
-				ensure_installed = {
-					'lua_ls',
-					'clangd',
-					'pyright',
-					'autotools_ls',
-					'marksman',
-				}
+				ensure_installed = langs
 			})
 		end
 	},
@@ -32,13 +49,31 @@ return {
 		config = function()
 			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-			require('mason-lspconfig').setup_handlers({
-				function(server_name)
-					require('lspconfig')[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-			})
+			-- require('mason-lspconfig').setup_handlers({
+			-- 	function(server_name)
+			-- 		require('lspconfig')[server_name].setup({
+			-- 			capabilities = capabilities,
+			-- 		})
+			-- 	end,
+			-- })
+
+			local lspconfig = require 'lspconfig'
+
+			for i, lang in ipairs(langs) do
+				local lang_config = {
+					capabilities = capabilities,
+				}
+
+				if lang == 'pyright' then
+					lang_config.settings = {
+						python = {
+							pythonPath = get_python_path(),
+						},
+					}
+				end
+
+				lspconfig[lang].setup(lang_config)
+			end
 
 			-- Key mappings
 			map_key('K', vim.lsp.buf.hover)
